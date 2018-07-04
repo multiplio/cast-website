f3bc05c24e7cfbe69ac056b7d884775385dbc37a
WhoMeNope <whomenope@outlook.com>
2018-07-04 20:02:01 +0200

    router reshuffle

--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
+import LoginC from "../Components/Login.js"
+
 export default class Login extends Component {
   render() {
     return (
       <div>
         Login
+        <LoginC/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import ReactDom from 'react-dom';
-import routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
+import Main from './Main.js'
 
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDom.render(
-  routes,
+  <BrowserRouter>
+  	<Main />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
