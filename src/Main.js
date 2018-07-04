37d4ed5d226977a4d1949bd47fe0c09079e3e8a8
WhoMeNope <whomenope@outlook.com>
2018-07-04 20:33:49 +0200

    add ToS & PP

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,9 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './Views/Home.js';
 import Login from './Views/Login.js';
 
+import PP from './Views/PrivacyPolicy.js';
+import TOS from './Views/TermsOfUse.js';
+
 export default class Main extends Component {
   render() {
     return (
@@ -11,6 +14,9 @@ export default class Main extends Component {
         <Switch>
 			    <Route exact path='/' component={Home} />
 			    <Route path='/login' component={Login} />
+          
+          <Route path='/privacy' component={PP} />
+          <Route path='/terms' component={TOS} />
 			  </Switch>
       </main>
     );
