0ef9e1ebddaee4e63c6a1f48cdf960f3592572e3
WhoMeNope <whomenope@outlook.com>
2018-10-23 16:33:37 +0200

    aphrodite jss

---
+++ src/Styles/pallete.js
@@ -0,0 +1,8 @@
+export default {
+  blue: 'rgba(91, 192, 235, 1)',
+  red: 'rgba(248, 51, 60, 1)',
+  yellow: 'rgba(247, 184, 1, 1)',
+
+  black: 'rgba(13, 2, 33, 1)',
+  white: 'rgba(237, 230, 227, 1)',
+};
