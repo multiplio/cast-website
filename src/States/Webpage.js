c69e912696a28326b639ce520de5e1aa4b7875ad
Martin Toman <whomenope@outlook.com>
2019-02-27 22:35:28 +0100

    remember cookie consent

--- src/States/Webpage.js
+++ src/States/Webpage.js
@@ -1,13 +1,19 @@
 import { observable, action, decorate } from 'mobx'
+import Cookies from 'universal-cookie'
+
+const cookiesCookie = 'cookies-accepted'
 
 class Webpage {
   constructor () {
+    this.cookies = new Cookies()
+
     // observables
-    this.accepted = false
+    this.accepted = this.cookies.get(cookiesCookie) || false
   }
 
   // actions
   acceptCookies () {
+    this.cookies.set(cookiesCookie, 'true', { path: '/' })
     this.accepted = true
   }
 }
