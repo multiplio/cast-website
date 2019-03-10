1861f3e645550cb847ee7e1bbfe89392a883dff1
Martin Toman <whomenope@outlook.com>
2019-03-10 13:30:48 +0100

    font stack, update color pallete

--- src/Styles/pallete.js
+++ src/Styles/pallete.js
@@ -1,9 +1,5 @@
 const pallete = {
-  blue: 'rgba(91, 192, 235, 1)',
-  red: 'rgba(248, 51, 60, 1)',
-  yellow: 'rgba(247, 184, 1, 1)',
-
-  background: 'rgba(227, 227, 227, 1)',
+  background: 'rgba(235, 235, 245, 1)',
 
   black: 'rgba(13, 2, 33, 1)',
   white: 'rgba(255, 255, 255, 1)',
