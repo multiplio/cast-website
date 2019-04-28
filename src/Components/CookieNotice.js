b81934800c8f7cb398dd0704b06899e0ab358cf9
Martin Toman <whomenope@outlook.com>
2019-04-28 17:01:30 +0200

    improve design of cookie notice - monochromatic

--- src/Components/CookieNotice.js
+++ src/Components/CookieNotice.js
@@ -15,13 +15,17 @@ class CookieNotice extends Component {
     else {
       return (
         <div className={css(styles.container)}>
-          <div className={css(styles.text)}>
-            This webpage uses cookies. By continuing, you agree to their use.
-          </div>
-          <div
-            className={css(styles.dismiss)}
-            onClick={ (event) => webpage.acceptCookies() }>
-            Got it!
+          <div className={css(styles.view)}>
+
+            <div className={css(styles.text)}>
+              This webpage uses cookies. By continuing, you agree to their use.
+            </div>
+            <div
+              className={css(styles.dismiss)}
+              onClick={ (event) => webpage.acceptCookies() }>
+              Got it!
+            </div>
+
           </div>
         </div>
       )
@@ -35,35 +39,40 @@ export default inject('webpage')(observer(CookieNotice))
 
 const styles = StyleSheet.create({
   container: {
+    'height': '5rem',
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    right: 0,
     display: 'flex',
-    'flex-direction': 'row',
+    'justify-content': 'space-around',
+    backgroundColor: 'white',
+  },
+  view: {
+    width: '100%',
+    'max-width': '1000px',
+    height: '100%',
+    display: 'flex',
     'align-items': 'center',
     'justify-content': 'space-between',
     'flex-wrap': 'nowrap',
-    backgroundColor: pallete.background,
-    position: 'fixed',
-    bottom: 0,
-    left: '5rem',
-    right: '5rem',
-    'height': '5rem',
-    padding: '0 0.5rem',
-    border: 'solid black 1px',
-    'border-radius': '10px 10px 0 0',
   },
+
   text: {
     'flex-grow': 1,
+    margin: '0 0 0 1.5rem',
   },
   dismiss: {
     'flex-basis': 'auto',
     'flex-shrink': 0,
     'white-space': 'nowrap',
-    backgroundColor: pallete.primary,
-    padding: '1rem',
+    padding: '0.5rem',
+    margin: '0 1.5rem 0 0',
     float: 'right',
     cursor: 'pointer',
-    'border-radius': '10px',
+    border: '3px solid ' + pallete.background,
     ':hover': {
-      backgroundColor: pallete.primaryDark,
+      'font-weight': 'bold',
     },
   },
 })
