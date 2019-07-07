f99242de6f2d50a39c52d1d03ce7591fa1908dd4
Martin Toman <whomenope@outlook.com>
2019-07-07 17:35:24 +0200

    Restructure how section

--- src/Components/Explanation.js
+++ src/Components/Explanation.js
@@ -3,17 +3,6 @@ import { StyleSheet, css } from 'aphrodite'
 
 import cluster from '../Assets/cluster.svg'
 
-import twitter from '../Assets/social/twitter.svg'
-import facebook from '../Assets/social/facebook.svg'
-import pinterest from '../Assets/social/pinterest.svg'
-import reddit from '../Assets/social/reddit.svg'
-import tumblr from '../Assets/social/tumblr.svg'
-import linkedin from '../Assets/social/linkedin.svg'
-import instagram from '../Assets/social/instagram.svg'
-import whatsapp from '../Assets/social/whatsapp.svg'
-
-// import pallete from '../Styles/pallete'
-
 const Explanaition = () => {
   return (
     <div className={css(styles.view)}>
@@ -29,28 +18,34 @@ const Explanaition = () => {
         <h1>How?</h1>
       </section>
 
+      <section className={css(styles.post)}>
+      </section>
+      <section className={css(styles.postText)}>
+        <div>
+          <h1>Post</h1>
+        </div>
+      </section>
+
       <section className={css(styles.storage)}>
         <img alt="" src={cluster} className={css(styles.storageImg)} />
       </section>
-
       <section className={css(styles.storageText)}>
         <div>
-          <h1>Safely Stored</h1>
+          <h1>Safelly Stored</h1>
           <p>Stored in an immutable distributed global storage, always safe, always available.</p>
         </div>
       </section>
 
-      <section className={css(styles.social)}>
-        <img alt="" src={twitter} className={css(styles.socialImage)} />
-        <img alt="" src={facebook} className={css(styles.socialImage)} />
-        <img alt="" src={pinterest} className={css(styles.socialImage)} />
-        <img alt="" src={reddit} className={css(styles.socialImage)} />
-        <img alt="" src={tumblr} className={css(styles.socialImage)} />
-        <img alt="" src={linkedin} className={css(styles.socialImage)} />
-        <img alt="" src={instagram} className={css(styles.socialImage)} />
-        <img alt="" src={whatsapp} className={css(styles.socialImage)} />
+      <section className={css(styles.publish)}>
+      </section>
+      <section className={css(styles.publishText)}>
+        <div>
+          <h1>Publish</h1>
+        </div>
       </section>
 
+      <section className={css(styles.social)}>
+      </section>
       <section className={css(styles.socialText)}>
         <div>
           <h1>All Your Social Media</h1>
@@ -71,7 +66,9 @@ const styles = StyleSheet.create({
     'grid-template': `
       "what what"
       "how how"
-      "storage storage-text"
+      "post post-text"
+      "storage-text storage"
+      "publish publish-text"
       "social-text social"
     `,
     'grid-template-columns': '50% 50%',
@@ -91,6 +88,24 @@ const styles = StyleSheet.create({
     'grid-area': 'how',
   },
 
+  post: {
+    'grid-area': 'post',
+    display: 'grid',
+    'grid-template-columns': '55px 55px 55px 55px',
+    'grid-template-rows': '55px 55px',
+    'grid-gap': '5px 5px',
+    'justify-content': 'center',
+    'align-content': 'center',
+  },
+  postText: {
+    'grid-area': 'post-text',
+    padding: '2rem',
+    display: 'flex',
+    'align-items': 'center',
+    'text-align': 'center',
+    'justify-content': 'space-around',
+  },
+
   storage: {
     'grid-area': 'storage',
     display: 'flex',
@@ -100,11 +115,28 @@ const styles = StyleSheet.create({
     'justify-content': 'space-around',
   },
   storageImg: {
-    'max-height': '80%',
+    'max-height': '100%',
   },
   storageText: {
     'grid-area': 'storage-text',
-    padding: '2rem 4rem',
+    padding: '2rem',
+    display: 'flex',
+    'align-items': 'center',
+    'text-align': 'center',
+    'justify-content': 'space-around',
+  },
+
+  publish: {
+    'grid-area': 'publish',
+    display: 'flex',
+    padding: '2rem',
+    'flex-direction': 'column',
+    'align-items': 'center',
+    'justify-content': 'space-around',
+  },
+  publishText: {
+    'grid-area': 'publish-text',
+    padding: '2rem',
     display: 'flex',
     'align-items': 'center',
     'text-align': 'center',
@@ -127,10 +159,6 @@ const styles = StyleSheet.create({
     'align-items': 'center',
     'text-align': 'center',
     'justify-content': 'space-around',
-  },
-  socialImage: {
-    width: '55px',
-    height: 'auto',
   },
 })
 
