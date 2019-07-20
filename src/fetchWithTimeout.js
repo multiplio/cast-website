1d21374f2b429cb53d47f94a59c385d329415d9c
Martin Toman <whomenope@outlook.com>
2019-07-20 15:32:24 +0200

    Cancel long publish requests

--- src/fetchWithTimeout.js
+++ src/fetchWithTimeout.js
@@ -1,15 +1,38 @@
+function abortAvailable (self) {
+  return !((typeof (self.Request) === 'function' && !self.Request.prototype.hasOwnProperty('signal')) || !self.AbortController)
+}
+
 function fetchWithTimeout (url, options, timeout) {
   return new Promise((resolve, reject) => {
+    let controller = null
+    let signal = null
+
+    if (abortAvailable(window)) {
+      controller = new window.AbortController()
+      signal = controller.signal
+    }
+
     // Set timeout timer
     let timer = setTimeout(
-      () => reject(new Error('Request timed out')),
+      () => {
+        if (controller !== null) {
+          controller.abort()
+        }
+      },
       timeout
     )
 
-    fetch(url, options)
+    fetch(url, { ...options, signal })
       .then(
         response => resolve(response),
-        err => reject(err)
+        err => {
+          if (err.name === 'AbortError') {
+            reject(new Error('Request timed out'))
+          }
+          else {
+            reject(err)
+          }
+        }
       )
       .finally(() => clearTimeout(timer))
   })
