43dc83f147936b00389b108b265ca18fb6b15e3c
Martin Toman <whomenope@outlook.com>
2019-07-20 13:20:14 +0200

    Add fetchWithTimeout

---
+++ src/fetchWithTimeout.js
@@ -0,0 +1,18 @@
+function fetchWithTimeout(url, options, timeout) {
+  return new Promise((resolve, reject) => {
+    // Set timeout timer
+    let timer = setTimeout(
+      () => reject(new Error('Request timed out')),
+      timeout
+    )
+
+    fetch(url, options)
+      .then(
+        response => resolve(response),
+        err => reject(err)
+      )
+      .finally(() => clearTimeout(timer))
+  })
+}
+
+export default fetchWithTimeout
